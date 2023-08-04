d5f1cc365ab4833634e1bd25f57233aba6090ea4
karkraz <rochadcmarcos@gmail.com>
2023-08-04 13:58:05 -0300

    change stadiums table

--- create_tables.sql
+++ create_tables.sql
@@ -53,8 +53,10 @@ CREATE TABLE coaches (
 CREATE TABLE stadiums ( 
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    location VARCHAR(100) NOT NULL,
-    capacity INT NOT NULL,
+    region VARCHAR(100),
+    country VARCHAR(100),
+    city VARCHAR(100),
+    capacity INT,
     club_owner VARCHAR(100)
 );
 
