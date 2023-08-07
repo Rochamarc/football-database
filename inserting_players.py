5fdc70ba98572ee4e60953ec528ee0a6aee1b2b7
karkraz <rochadcmarcos@gmail.com>
2023-08-06 21:12:16 -0300

    separate player creation from insertion

--- inserting_players.py
+++ inserting_players.py
@@ -1,6 +1,5 @@
 import mysql.connector
-from random import randint, choice, uniform 
-
+from creating_players import create_players_data
 
 query = 'INSERT INTO players(name, nationality, age, position, height, weight, foot, retired) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
 
@@ -24,42 +23,6 @@ def insert_players(players: list) -> None:
 
     return None
 
-def define_name() -> str:
-    ''' Define players name '''
-    name = ''
-    
-    with open('files/players/first_names.csv', 'r') as f:
-        line = f.readlines()
-        line = [ i.split(',') for i in line ]
-        name += choice([ i[0] for i in line ])
-    
-    name += ' '
-    
-    with open('files/players/last_names.csv', 'r') as f:
-        line = f.readlines()
-        line = [ i.split(',') for i in line ]
-        name += choice([ i[0] for i in line ])
-        
-    return name 
-
-
-def player_data() -> list:
-    nationality = choice(['Brazil', 'Argentina', 'Bolivia', 'Uruguay' ]) # for while
-    
-    name = define_name()
-    
-    age = randint(15,40)
-    positions = choice([ 'GK', 'RB', 'CB', 'LB', 'DM', 'CM', 'AM', 'RM', 'LM', 'RW', 'LW', 'SS', 'CF' ])
-    height =  float("{0:.2f}".format(uniform(1.6,1.9)))
-    weight = float("{0:.2f}".format(uniform(60,90)))
-    foot = choice(choice(['R', 'L']))
-    retired = 'F'
-
-    return [ name, nationality, age, positions, height, weight, foot, retired ]
-
-players = []
-
-for _ in range(10000):
-    players.append(player_data())
-
-insert_players(players)
+if __name__ == "__main__":
+    players = create_players_data(10000)
+    insert_players(players)
