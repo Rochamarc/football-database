aa7fe2d0776b2c3ef2b07c34747fc6a9c8b1b082
karkraz <rochadcmarcos@gmail.com>
2023-08-10 00:20:51 -0300

    some jupyter files

---
+++ .ipynb_checkpoints/inserting_clubs-checkpoint.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+query = 'INSERT INTO clubs (name, country, coeff, class) VALUES (%s,%s,%s,%s);'
+
+database_config = {
+    'user': 'tournament_user',
+    'host': 'localhost',
+    'password': 'tournament_pass',
+    'database': 'football'
+}
+
+def insert_clubs_db(clubs: list) -> None:
+    '''
+    Insert clubs into the database
+    '''
+    conn = mysql.connector.connect(**database_config)
+    cursor = conn.cursor()
+
+    for club in clubs:
+        cursor.execute(query, club)
+
+    conn.commit()
+    conn.close()
+
+    return None
+            
