fc03bf97a1e6d822fe4c06f1fec21b9f95483a88
karkraz <rochadcmarcos@gmail.com>
2023-08-20 01:26:47 -0300

    delete insert overall and change select players

--- queries/select_players_clubs_and_contracts.sql
+++
@@ -1,1 +0,0 @@
-SELECT clubs.name, players.name, players.position, players.age, players_contract.value, players_contract.salary, players_contract.termination_fine FROM clubs INNER JOIN players_contract INNER JOIN players ON clubs.id = id_club and players.id = id_player;
