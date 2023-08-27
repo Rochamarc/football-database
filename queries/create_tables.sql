6ab32d8abe9859aca18c6913e24728f805f9a00e
karkraz <rochadcmarcos@gmail.com>
2023-08-27 03:45:07 -0300

    create table games

--- queries/create_tables.sql
+++ queries/create_tables.sql
@@ -170,6 +170,51 @@ CREATE TABLE player_values_season(
         REFERENCES players(id)
 );
 
+CREATE TABLE home_game_stats(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    shots INT NOT NULL,
+    shots_on_target INT NOT NULL,
+    fouls INT NOT NULL,
+    tackles INT NOT NULL,
+    saves INT NOT NULL,
+    ball_possession INT NOT NULL,
+    offsides INT NOT NULL,
+    freekicks INT NOT NULL
+);
+
+CREATE TABLE away_game_stats(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    shots INT NOT NULL,
+    shots_on_target INT NOT NULL,
+    fouls INT NOT NULL,
+    tackles INT NOT NULL,
+    saves INT NOT NULL,
+    ball_possession INT NOT NULL,
+    offsides INT NOT NULL,
+    freekicks INT NOT NULL
+);
+
+-- Games
+CREATE TABLE games(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    season CHAR(4) NOT NULL,
+    hour CHAR(5),
+    score VARCHAR(5),
+    id_competition INT,
+    FOREIGN KEY (id_competition)
+        REFERENCES competitions(id),
+    id_stadium INT,
+    FOREIGN KEY (id_stadium)
+        REFERENCES stadiums(id),
+    id_home_game_stats INT,
+    FOREIGN KEY (id_home_game_stats)
+        REFERENCES home_game_stats(id),
+    id_away_game_stats INT,
+    FOREIGN KEY (id_away_game_stats)
+        REFERENCES away_game_stats(id)
+);
+
+
 /* INSERT EXAMPLES */
 -- CLUBS
 INSERT INTO clubs(name, country, coeff, class) VALUES ('Fluminense FC', 'Brasil', 1000, 'A');
