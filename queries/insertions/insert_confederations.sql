5550a3807d322542e9a5d2fea32b13ec4a287803
karkraz <rochadcmarcos@gmail.com>
2023-08-30 16:16:11 -0300

    change file name

--- queries/insertions/insert_confederations.sql
+++ queries/insertions/insert_confederations.sql
@@ -20,7 +20,7 @@ INSERT INTO national_confederations VALUES (NULL, 'FVF','Venezuela');
 SELECT id FROM international_confederations WHERE continent='South America'; -- 1
 
 UPDATE clubs 
-SET id_confederation=1 
+SET id_international_confederation=1 
 WHERE 
 country='Brazil' OR country='Argentina' OR country='Peru';
 
@@ -28,4 +28,33 @@ country='Brazil' OR country='Argentina' OR country='Peru';
 SELECT international_confederations.name, clubs.name, clubs.country 
 FROM clubs
 INNER JOIN international_confederations
-    ON id_confederation = international_confederations.id;
+    ON id_international_confederation = international_confederations.id;
+
+
+SELECT * FROM national_confederations;
++----+------+-----------+----------+
+| id | name | country   | Changes  |
++----+------+-----------+----------+
+|  1 | AFA  | Argentina |          |
+|  2 | FBF  | Bolívia   | Bolivia  |
+|  3 | CBF  | Brasil    | Brazil   |
+|  4 | FFC  | Chile     |          |
+|  5 | FCF  | Colômbia  | Colombia |
+|  6 | FEF  | Equador   | Ecuador  |
+|  7 | APF  | Paraguai  | Paraguay |
+|  8 | FPF  | Peru      |          |  
+|  9 | AUF  | Uruguai   | Uruguay  |
+| 10 | FVF  | Venezuela |          |
++----+------+-----------+----------+
+
+UPDATE clubs
+SET id_national_confederation = 3
+WHERE clubs.country = 'Brazil';
+
+
+-- Query of brazilian clubs
+SELECT * FROM clubs 
+INNER JOIN international_confederations 
+INNER JOIN national_confederations 
+    ON clubs.id_international_confederation = international_confederations.id 
+    AND clubs.id_national_confederation = national_confederations.id;
