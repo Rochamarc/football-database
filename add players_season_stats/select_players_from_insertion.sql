547b2c54eb40b8ec196da8ebad506a9624465cb1
karkraz <rochadcmarcos@gmail.com>
2023-09-06 22:09:03 -0300

    remove players season stats
    this code already have a notebook that deals with his function

--- add players_season_stats/select_players_from_insertion.sql
+++
@@ -1,7 +0,0 @@
-SELECT players.id, players.position, players_overall.overall 
-FROM players
-INNER JOIN players_contract
-INNER JOIN players_overall
-    ON players.id = players_contract.id_player
-    AND players.id = players_overall.id_player
-WHERE players_overall.season = '2023';
