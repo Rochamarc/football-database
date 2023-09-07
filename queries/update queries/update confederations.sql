9854c277cbc765b1371b2f4c7ea6ffb04a8e2520
karkraz <rochadcmarcos@gmail.com>
2023-09-06 23:38:53 -0300

    update confederations

--- queries/update queries/update confederations.sql
+++ queries/update queries/update confederations.sql
@@ -1,3 +1,14 @@
+/*
+UPDATE CLUBS INTERNATIONAL FEDERATION 
+FROM SOUTH AMERICAN CLUBS
+*/
+
+UPDATE clubs 
+INNER JOIN international_confederations
+    ON international_confederations.continent='South America'
+SET clubs.id_international_confederation = international_confederations.id
+WHERE clubs.country IN ('Argentina','Bolivia','Brazil','Chile','Colombia','Ecuador','Paraguay','Peru','Uruguay','Venezuela');
+
 /* 
 UPDATE CLUBS.NATIONAL_CONFEDERATIONS BASED ON INNER JOIN 
 
@@ -11,9 +22,7 @@ SET clubs.id_national_confederation = national_confederations.id;
 
 /*
 SEELCT CLUBS NAME, INTERNATIONAL FEDERATION NAME AND NATIONAL FEDERATION NAME
-
 */
-
 SELECT clubs.name, international_confederations.name, national_confederations.name 
 FROM clubs 
 INNER JOIN international_confederations
