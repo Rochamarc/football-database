39c62831c90b1868a3c9253c3ea5de82386a858a
karkraz <rochadcmarcos@gmail.com>
2023-10-10 00:41:44 -0300

    notebook checkopoints

---
+++ queries/insertions/.ipynb_checkpoints/insert_confederations-checkpoint.sql
@@ -0,0 +1,19 @@
+/* International Federations */
+INSERT INTO international_confederations VALUES (NULL, 'CONMEBOL', 'South America');
+INSERT INTO international_confederations VALUES (NULL, 'CONCACAF', 'North America');
+INSERT INTO international_confederations VALUES (NULL, 'UEFA', 'Europe');
+INSERT INTO international_confederations VALUES (NULL, 'CAF', 'Africa');
+INSERT INTO international_confederations VALUES (NULL, 'AFC', 'Asia');
+INSERT INTO international_confederations VALUES (NULL, 'OFC', 'Oceania');
+
+/* South American Federations */
+INSERT INTO national_confederations VALUES (NULL, 'AFA','Argentina');
+INSERT INTO national_confederations VALUES (NULL, 'FBF','Bolivia');
+INSERT INTO national_confederations VALUES (NULL, 'CBF','Braszl');
+INSERT INTO national_confederations VALUES (NULL, 'FFC','Chile');
+INSERT INTO national_confederations VALUES (NULL, 'FCF','Colombia');
+INSERT INTO national_confederations VALUES (NULL, 'FEF','Ecuador');
+INSERT INTO national_confederations VALUES (NULL, 'APF','Paraguay');
+INSERT INTO national_confederations VALUES (NULL, 'FPF','Peru');
+INSERT INTO national_confederations VALUES (NULL, 'AUF','Uruguay');
+INSERT INTO national_confederations VALUES (NULL, 'FVF','Venezuela'); 
